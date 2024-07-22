93429e71c84b053a15711ca34d4ebb766ccb4a3a
Sayat <127617769+SayatAbdikul@users.noreply.github.com>
2024-07-22 16:11:26 +0500

    a little testbench

--- level_4/lab_3/obj_dir/tb_bitty.d
+++ level_4/lab_3/obj_dir/tb_bitty.d
@@ -4,6 +4,6 @@ tb_bitty.o: ../tb_bitty.cpp \
   /opt/homebrew/Cellar/verilator/5.026/share/verilator/include/verilated_config.h \
   /opt/homebrew/Cellar/verilator/5.026/share/verilator/include/verilated_types.h \
   /opt/homebrew/Cellar/verilator/5.026/share/verilator/include/verilated_funcs.h \
-  VControlUnit.h \
+  Vtop_module.h \
   /opt/homebrew/Cellar/verilator/5.026/share/verilator/include/verilated_vcd_c.h \
   /opt/homebrew/Cellar/verilator/5.026/share/verilator/include/verilated_trace.h
