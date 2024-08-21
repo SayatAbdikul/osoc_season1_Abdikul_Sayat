1c9150f1d42a7725c1809b5194539a32cccbade3
Sayat <127617769+SayatAbdikul@users.noreply.github.com>
2024-08-21 18:49:55 +0500

    changed the syntax of the assembler

--- level_4/bitty_processor/assembler_tool/Assembler_tool.cpp
+++ level_4/bitty_processor/assembler_tool/Assembler_tool.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <cctype>
 #include "Assembler_tool.h"
 int getRange(int x, int y, int instruction) {
     if (x < 0 || y > 15 || x > y) {
@@ -16,12 +17,13 @@ int Assembler_tool::Assemble(std::string instruction){
     std::string command = ""; 
     int i=0, x=0, y=0, p=0;
     while(instruction[i]!=' ') command += instruction[i], i++;
-    while(instruction[i]==' ') i++;
-    while(instruction[i]!=',') x+=(instruction[i]-'0')*pow(10, p), p++, i++;
+    while(instruction[i]==' ' || instruction[i]=='r') i++;
+    while(instruction[i]!=' ') x+=(instruction[i]-'0')*pow(10, p), p++, i++;
     p=0;
     i+=2;
     while(instruction[i]!='\n' && instruction[i]!=' ' && i<instruction.length()) y+=(instruction[i]-'0')*pow(10, p), p++, i++;
     int result=0, select=0;
+    for(int i=0; i<command.length(); i++) command[i] = toupper(command[i]);
     for(int i=0; i<8; i++) if(commands[i]==command) {select=i; break;}
     result += (x<<13);
     result += (y<<10);
@@ -31,8 +33,8 @@ int Assembler_tool::Assemble(std::string instruction){
 }
 std::string Assembler_tool::Disassemble(int instruction){
     std::string command = "";
-    command += commands[getRange(2, 4, instruction)]+" ";
-    command += std::to_string(getRange(13, 15, instruction))+", ";
+    command += commands[getRange(2, 4, instruction)]+" r";
+    command += std::to_string(getRange(13, 15, instruction))+" r";
     command += std::to_string(getRange(10, 12, instruction));
     std::cout<<"from "<<instruction<<" to "<<command<<"\n";
     return command;
